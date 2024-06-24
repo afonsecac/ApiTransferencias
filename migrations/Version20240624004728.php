0e11b23a2a633d8face45a878ad2d9698a986cc2
Alexander Fonseca Cardosa <alexander.afonsecac@gmail.com>
2024-06-24 06:20:22 -0400

    Information request

--- migrations/Version20240624004728.php
+++ migrations/Version20240624004728.php
@@ -23,9 +23,6 @@ final class Version20240624004728 extends AbstractMigration
         $this->addSql('ALTER TABLE communication_promotions ADD environment_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE communication_promotions ADD CONSTRAINT FK_3EA2E68E903E3A94 FOREIGN KEY (environment_id) REFERENCES environment (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('CREATE INDEX IDX_3EA2E68E903E3A94 ON communication_promotions (environment_id)');
-        $this->addSql('ALTER TABLE env_auth ALTER token_auth TYPE TEXT');
-        $this->addSql('ALTER TABLE env_auth ALTER token_auth TYPE TEXT');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_98029F8D9ADA853B ON env_auth (token_auth)');
     }
 
     public function down(Schema $schema): void
