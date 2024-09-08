af97d9288c70222beda47657d79bdc9f028c399b
Alexander Fonseca <alexander.afonsecac@gmail.com>
2024-09-08 19:49:53 +0200

    Email is active

--- src/Repository/EmailNotificationRepository.php
+++ src/Repository/EmailNotificationRepository.php
@@ -32,28 +32,4 @@ class EmailNotificationRepository extends ServiceEntityRepository
             ]))
             ->getQuery()->setMaxResults(1)->getOneOrNullResult();
     }
-    //    /**
-    //     * @return EmailNotification[] Returns an array of EmailNotification objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('e.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?EmailNotification
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
