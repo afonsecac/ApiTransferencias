51f64d4f9c199d825e47073a13ca78cbc453b0f3
Alexander Fonseca <alexander.afonsecac@gmail.com>
2024-09-18 18:42:48 +0200

    Sales info

--- migrations/Version20240915102122.php
+++ migrations/Version20240915102122.php
@@ -20,8 +20,6 @@ final class Version20240915102122 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE identification_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE identification_type (id INT NOT NULL, PRIMARY KEY(id))');
         $this->addSql('ALTER TABLE communication_sale_info ADD identification_type INT DEFAULT NULL');
     }
 
@@ -29,8 +27,6 @@ final class Version20240915102122 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE identification_type_id_seq CASCADE');
-        $this->addSql('DROP TABLE identification_type');
         $this->addSql('ALTER TABLE communication_sale_info DROP identification_type');
     }
 }
