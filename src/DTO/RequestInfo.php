50590ed98ae96aa8b9db5531e4590e1bf660220c
Alexander Fonseca <alexander.afonsecac@gmail.com>
2025-09-24 06:43:40 +0200

    Sale Request Info

--- src/DTO/RequestInfo.php
+++ src/DTO/RequestInfo.php
@@ -8,14 +8,14 @@ final class RequestInfo implements IInput
 {
     private string $type;
     private ?string $clientTxId;
-    private ?string $internalTxId;
+    private ?int $internalTxId;
 
     /**
      * @param string $type
      * @param string|null $clientTxId
      * @param string|null $internalTxId
      */
-    public function __construct(string $type, ?string $clientTxId, ?string $internalTxId)
+    public function __construct(string $type, ?string $clientTxId, ?int $internalTxId)
     {
         $this->type = $type;
         $this->clientTxId = $clientTxId;
@@ -42,12 +42,12 @@ final class RequestInfo implements IInput
         $this->clientTxId = $clientTxId;
     }
 
-    public function getInternalTxId(): ?string
+    public function getInternalTxId(): ?int
     {
         return $this->internalTxId;
     }
 
-    public function setInternalTxId(?string $internalTxId): void
+    public function setInternalTxId(?int $internalTxId): void
     {
         $this->internalTxId = $internalTxId;
     }
